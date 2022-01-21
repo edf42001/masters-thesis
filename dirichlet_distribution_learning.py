40ad14af421c2ee83e144a25ca50517424c107b4
Ethan Frank <edf32@case.edu>
2022-01-21 16:15:06 -0500

    Sample mdp from parameters

--- dirichlet_distribution_learning.py
+++ dirichlet_distribution_learning.py
@@ -4,6 +4,9 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
+# Make it easier to read everything
+np.set_printoptions(precision=2, suppress=True)
+
 
 def transition_hash(s, a, s_p):
     # Parameters of the MDP are for each state, action, next state probability
@@ -22,6 +25,17 @@ def reward_hash(s, a, r):
     return s + \
            a * NUM_STATES + \
            r * NUM_STATES * NUM_ACTIONS
+
+
+# Need to sample mdp from parameters:
+def sample_mdp_from_params(params, num_states, num_actions):
+    # For each transition, need to sample separately
+    mdp_sample = np.zeros_like(params)
+    for i in range(params.shape[0]):
+        for j in range(params.shape[1]):
+            mdp_sample[i, j, :] = np.random.dirichlet(params[i, j, :])
+
+    return mdp_sample
 
 
 env = LineEnv()
@@ -72,16 +86,50 @@ while iterations < TRAINING_EPOCHS:
 # Interesting, not the reshape goes in the opposite direction from the order the multiplication for the hash is done
 # print(transition_parameter_table.reshape((NUM_STATES, NUM_ACTIONS, NUM_STATES)))
 # print(reward_parameter_table.reshape((NUM_REWARDS, NUM_ACTIONS, NUM_STATES)))
+print("Parameter tables detailing reward and transition counts")
 print(transition_parameter_table)
 print(reward_parameter_table)
 
 # To extract the transition probabilites for a single state/action pair,
 # take the subset of transition_dirichlet_alphas like so:
-mdp_params = transition_dirichlet_alphas[3, 0, :].reshape(-1)
-mdp_sample = np.random.dirichlet(mdp_params)
 
-print(mdp_params)
-print(mdp_sample)
+# Generate samples:
+print("Sampled mdps:")
+print(transition_dirichlet_alphas)
+for i in range(4):
+    mdp_sample = sample_mdp_from_params(transition_dirichlet_alphas, NUM_STATES, NUM_ACTIONS)
+    print(mdp_sample)
+
 
 # Could also sample whole thing at once, then normalize?
 # Or is each transition probability supposed to be independent?
+
+
+# Work on value of imperfect information here
+# If bad action becomes better, gain is q*(s,a) - E[q(s, a)]
+# If a good action becomes worse, gain of performing other action is E[q(s, a2)] - q*(s, a1))
+# Cost of this action is difference between it and best action
+# Thus, try to maximize VPI(s, a) - max_a'(E(q) - stuff)
+# Because max action value is always the same, try to maximize
+# E[q(s, a)] + VPI(s, a)
+
+def calc_gain(s, a, new_learned_q):
+    # a1 and a2 are best and second best actions
+    # TODO
+    if a == "BEST ACTION":
+        # and q is less than second best (otherwise best would still be best)
+        gain = "expected q of second value" - new_learned_q
+    elif a != "BEST ACTION":
+        # and new value is better than best action (otherwise it would stay the same)
+        gain = new_learned_q - "expected q of best value"
+    else:
+        gain = 0
+
+    return gain
+
+
+def calc_value_perfect_information(s, a):
+    # Sum of gains over all different values the new q values could be (x)
+    # weighted by the probability that it is that value
+    # TODO
+    return 0
