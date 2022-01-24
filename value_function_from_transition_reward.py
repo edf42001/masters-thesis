48d2e018ffb0e83e36e689344575f2388e8bc44a
Ethan Frank <edf32@case.edu>
2022-01-24 10:38:42 -0500

    Update value function from transitions input data structure

--- value_function_from_transition_reward.py
+++ value_function_from_transition_reward.py
@@ -1,90 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# This script is to practice writing code to calculate (with dynamic programming)
-# The value funcition of states/actions (q function) of the simple chain environemt
-# First, we need a data structure to hold the data. How about an array of states, 1-N, each entry a list of tuples,
-# each tuple a next state, and another list for rewards? These are sparse, so anything not mentioned does not exist.
-# I include the first state in the tuple just for fun
 
-NUM_STATES = 5
-NUM_ACTIONS = 2
+def q_values_from_transition_reward_iterative(transitions, rewards, discount):
+    # This script is to practice writing code to calculate (with dynamic programming)
+    # The value funcition of states/actions (q function) of the simple chain environemt
+    # First, we need a data structure to hold the data:
+    # Transitions and rewards are just big 3D arrays for s, a, next state / reward
 
-# Hashed index method. Good if not sparse?
-transitions = [
-    # Action 0 transition probabilities
-    # (next_state, prob)
-    [(1, 0.8), (0, 0.2)],
-    [(2, 0.8), (0, 0.2)],
-    [(3, 0.8), (0, 0.2)],
-    [(4, 0.8), (0, 0.2)],
-    [(4, 0.8), (0, 0.2)],
-
-    # Action 1 transition probabilities
-    [(0, 0.8), (1, 0.2)],
-    [(0, 0.8), (2, 0.2)],
-    [(0, 0.8), (3, 0.2)],
-    [(0, 0.8), (4, 0.2)],
-    [(0, 0.8), (4, 0.2)],
-]
-
-rewards = [
-    # Action 0 reward probabilities
-    # (reward, prob)
-    [(0, 0.8), (2, 0.2)],
-    [(0, 0.8), (2, 0.2)],
-    [(0, 0.8), (2, 0.2)],
-    [(0, 0.8), (2, 0.2)],
-    [(10, 0.8), (2, 0.2)],
-
-    # Action 1 transition probabilities
-    [(2, 0.8), (0, 0.2)],
-    [(2, 0.8), (0, 0.2)],
-    [(2, 0.8), (0, 0.2)],
-    [(2, 0.8), (0, 0.2)],
-    [(2, 0.8), (10, 0.2)],
-]
+    # Solve this with DP: Iteratively go over each state and do:
+    # Q(s, a) = E[R(s, a)] + discount * sum_over_next_states(T(s, a, s')*max_over_a(Q(s', a))
 
 
-def sa_hash(state, action):
-    # Index into a 10 size array
-    return state + NUM_STATES * action
+    NUM_STATES = transitions.shape[0]
+    NUM_ACTIONS = transitions.shape[1]
+    NUM_REWARDS = rewards.shape[2]
+
+    q_table = np.zeros(NUM_STATES * NUM_ACTIONS)
+
+    NUM_ITERATIONS = 50  # Iterations to do
+    iterations = 0
+
+    q_tables = np.zeros((NUM_ITERATIONS, 10))
+
+    while iterations < NUM_ITERATIONS:
+        # Each iteration, iterate over each action in the q table
+        # from front to back. Does order matter?
+        for i in range(len(q_table)):
+            s = i % 5
+            a = int(i / 5)
+            # print(s, a)
+            # The state/action pair is encoded in i, which can be thought of as already hashed
+
+            # Calculate expected reward
+            # For the state/action pair, extract the reward probabilities and values to calculate expected reward
+            # Reward value is the same as the prob index so multiply the probs by the nums 0 to N
+            s_a_reward = rewards[s, a, :]
+            expected_reward = sum(s_a_reward * np.arange(0, len(s_a_reward)))
+
+            # Next, sum expected q values over next states probabilistically
+            next_state_probs = transitions[s, a, :]
+            # print(next_state_probs)
+            # TODO: Make max of next states work for when there are more than two next states
+            next_average_q_value = sum([next_state_probs[i] * max(q_table[[i, i+NUM_STATES]]) for i in range(NUM_STATES)])
+            q_table[i] = expected_reward + discount * next_average_q_value
+
+        # Store q table history
+        q_tables[iterations] = q_table
+
+        iterations += 1
 
 
-# Solve this with DP: Iteratively go over each state and do:
-# Q(s, a) = E[R(s, a)] + discount * sum_over_next_states(T(s, a, s')*max_over_a(Q(s', a))
-q_table = np.zeros(10)
-
-NUM_ITERATIONS = 100  # Iterations to do
-discount = 0.90
-iterations = 0
-
-q_tables = np.zeros((NUM_ITERATIONS, 10))
-
-while iterations < NUM_ITERATIONS:
-    # Each iteration, iterate over each action in the q table
-    # from front to back. Does order matter?
-    for i in range(len(q_table)):
-        # The state/action pair is encoded in i, which can be thought of as already hashed
-
-        # Calculate expected reward
-        # For the state/action pair, extract the reward probabilites and values to calculate expected reward
-        expected_reward = sum([reward[0] * reward[1] for reward in rewards[i]])
-
-        # Next, sum rewards over next states probabilistically
-        transition = transitions[i]
-        # TODO: Make max of next states work for when there are more than two next states
-        next_expected_q_value = sum([prob * max(q_table[sa_hash(next_state, 0)], q_table[(sa_hash(next_state, 1))])
-                                     for (next_state, prob) in transition])
-        q_table[i] = expected_reward + discount * next_expected_q_value
-
-    # Store q table history
-    q_tables[iterations] = q_table
-
-    iterations += 1
-
-plt.plot(q_tables)
-plt.title("Q Table values over iterations")
-plt.show()
-
-
+if __name__ == "__main__":
+    print("No op for now")
+    # plt.plot(q_tables)
+    # plt.title("Q Table values over iterations")
+    # plt.show()
