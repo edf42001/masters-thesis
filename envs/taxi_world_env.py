ac8ce54837aab95daf5cb4e814fc83bfd30f37b4
Ethan Frank <edf32@case.edu>
2022-02-02 15:48:53 -0500

    Program taxi world

--- envs/taxi_world_env.py
+++ envs/taxi_world_env.py
@@ -1,6 +1,7 @@
 import random
 from enum import Enum
 import numpy as np
+import cv2
 
 
 # Enum for actions
@@ -22,6 +23,10 @@ class TaxiWorldEnv(object):
     """
 
     def __init__(self):
+        # Width and height in grid cells
+        self.WIDTH = 5
+        self.HEIGHT = 5
+
         # Taxi's position
         self.taxi_x = 0
         self.taxi_y = 0
@@ -38,16 +43,29 @@ class TaxiWorldEnv(object):
 
         # Location of walls
         self.walls_vertical = np.zeros((6, 6), dtype=bool)
-        self.horizontal = np.zeros((6, 6), dtype=bool)
+        self.walls_horizontal = np.zeros((6, 6), dtype=bool)
+        self.setup_walls()
 
         # Randomize starting values
         self.reset_passenger_pickup_dropoff()
         self.reset_taxi()
 
     def setup_walls(self):
-        # Wall count starts at 0, 0, and moves horizontally and up
+        # Walls are indexed by their bottom left corner. A horizontal wall and veritcal wall extend right and up from
+        # that point if they are marked as existing. This means there are (Width+1)*(Height+1) points, but the rightmost
+        # and upmost points aren't used for horizontal and vertical respectively.
         vertical = [0, 1, 3, 5, 6, 7, 9, 11, 12, 17, 18, 20, 23, 24, 26, 29]
-        horizontal = [0, 1, 2, 3, 4, 25, 26, 27, 28, 29]
+        horizontal = [0, 1, 2, 3, 4, 30, 31, 32, 33, 34]
+
+        for wall in vertical:
+            x = wall % (self.WIDTH + 1)
+            y = int(wall / (self.WIDTH + 1))
+            self.walls_vertical[x, y] = True
+
+        for wall in horizontal:
+            x = wall % (self.WIDTH + 1)
+            y = int(wall / (self.WIDTH + 1))
+            self.walls_horizontal[x, y] = True
 
     def step(self, action):
         # Negative reward by default
@@ -57,28 +75,32 @@ class TaxiWorldEnv(object):
         done = False
 
         # State changes based on action (deterministically)
-        # Using .value allows us to use numbers. TODO: use enums?
-        # TODO: Add effect of walls
-        if action == ACTION.NORTH.value:
+        # Using  allows us to use numbers. TODO: use enums?
+        if action in [ACTION.NORTH, ACTION.EAST, ACTION.SOUTH, ACTION.WEST] and \
+           self.touching_wall(action):
+            # If the action is trying to move but there is a wall in the way, literally do nothing
+            pass
+
+        elif action == ACTION.NORTH:
             self.taxi_y += 1
 
-        elif action == ACTION.EAST.value:
+        elif action == ACTION.EAST:
             self.taxi_x += 1
 
-        elif action == ACTION.SOUTH.value:
+        elif action == ACTION.SOUTH:
             self.taxi_y += -1
 
-        elif action == ACTION.WEST.value:
+        elif action == ACTION.WEST:
             self.taxi_x += -1
 
-        elif action == ACTION.PICKUP.value:
+        elif action == ACTION.PICKUP:
             square = self.stops[self.current_pickup]
 
             # If on the right spot and the passenger is there, pick them up
             if square[0] == self.taxi_x and square[1] == self.taxi_y and not self.passenger_in_taxi:
                 self.passenger_in_taxi = True
 
-        elif action == ACTION.DROPOFF.value:
+        elif action == ACTION.DROPOFF:
             square = self.stops[self.current_dropoff]
 
             # If on the right spot and the passenger is in taxi, drop them off, +10 reward
@@ -110,6 +132,74 @@ class TaxiWorldEnv(object):
         self.taxi_x = np.random.randint(0, 4)
         self.taxi_y = np.random.randint(0, 4)
 
+    def draw_taxi(self):
+        GRID_SIZE = 100
+        WIDTH = self.WIDTH
+        HEIGHT = self.HEIGHT
+
+        # Blank white square
+        img = 255 * np.ones((HEIGHT * GRID_SIZE, WIDTH * GRID_SIZE, 3))
+
+        # Draw pickup and dropoff zones
+        colors = {"R": [0, 0, 255], "G": [0, 255, 0], "B": [255, 0, 0], "Y": [0, 128, 128]}
+        for color in colors.keys():
+            location = self.stops[color]
+            bottom_left_x = location[0] * GRID_SIZE
+            bottom_left_y = (HEIGHT - location[1]) * GRID_SIZE
+            cv2.rectangle(img, (bottom_left_x, bottom_left_y), (bottom_left_x + GRID_SIZE, bottom_left_y - GRID_SIZE),
+                          thickness=-1, color=colors[color])
+
+        # Mark goal with small circle
+        goal_x = self.stops[self.current_dropoff][0]
+        goal_y = self.stops[self.current_dropoff][1]
+        cv2.circle(img, (int((goal_x + 0.5) * GRID_SIZE), int((HEIGHT - (goal_y + 0.5)) * GRID_SIZE)), int(GRID_SIZE * 0.05),
+                   thickness=-1, color=[0, 0, 0])
+
+        # Draw taxi
+        cv2.circle(img, (int((self.taxi_x + 0.5) * GRID_SIZE), int((HEIGHT - (self.taxi_y + 0.5)) * GRID_SIZE)), int(GRID_SIZE * 0.3),
+                   thickness=-1, color=[0, 0, 0])
+
+        # Draw passenger
+        if self.passenger_in_taxi:
+            pass_x = self.taxi_x
+            pass_y = self.taxi_y
+        else:
+            pass_x = self.stops[self.current_pickup][0]
+            pass_y = self.stops[self.current_pickup][1]
+
+        cv2.circle(img, (int((pass_x + 0.5) * GRID_SIZE), int((HEIGHT - (pass_y + 0.5)) * GRID_SIZE)), int(GRID_SIZE * 0.2),
+                   thickness=-1, color=[0.5, 0.5, 0.5])
+
+        # Draw horizontal and vertical walls
+        for i in range((self.WIDTH + 1) * (self.HEIGHT + 1)):
+            x = i % (self.WIDTH + 1)
+            y = int(i / (self.WIDTH + 1))
+
+            if self.walls_vertical[x, y]:
+                cv2.line(img, (x * GRID_SIZE, (HEIGHT - y) * GRID_SIZE), (x * GRID_SIZE, (HEIGHT - (y+1)) * GRID_SIZE),
+                         thickness=3, color=[0, 0, 0])
+
+            if self.walls_horizontal[x, y]:
+                cv2.line(img, (x * GRID_SIZE, (HEIGHT - y) * GRID_SIZE), ((x+1) * GRID_SIZE, (HEIGHT - y) * GRID_SIZE),
+                         thickness=3, color=[0, 0, 0])
+
+        cv2.imshow("Taxi World", img)
+        cv2.waitKey(2)
+
+    def touching_wall(self, direction):
+        """Checks if the taxi is touching a wall in a certain direction"""
+        if direction == ACTION.NORTH:
+            return self.walls_horizontal[self.taxi_x, self.taxi_y + 1]
+        elif direction == ACTION.EAST:
+            return self.walls_vertical[self.taxi_x+1, self.taxi_y]
+        elif direction == ACTION.SOUTH:
+            return self.walls_horizontal[self.taxi_x, self.taxi_y]
+        elif direction == ACTION.WEST:
+            return self.walls_vertical[self.taxi_x, self.taxi_y]
+        else:
+            print("Bad direction: {}".format(direction))
+            return False
+
     def get_state(self):
         return self.state
 
@@ -126,17 +216,20 @@ class TaxiWorldEnv(object):
 if __name__ == "__main__":
     env = TaxiWorldEnv()
 
+    # TODO: Way to automate this?
+    action_map = [ACTION.NORTH, ACTION.EAST, ACTION.SOUTH, ACTION.WEST, ACTION.PICKUP, ACTION.DROPOFF]
+
     NUM_STEPS = 20
     iterations = 0
     while iterations < NUM_STEPS:
 
-        action = int(input("Enter action: "))
+        env.draw_taxi()
+        action = action_map[int(input("Enter action: "))]
         reward, done = env.step(action)
 
         print("Reward: {}".format(reward))
         print("pickup: {}, dropoff: {}".format(env.current_pickup, env.current_dropoff))
         print("x: {}, y: {}".format(env.taxi_x, env.taxi_y))
         print("passneger_in_taxi: {}".format(env.passenger_in_taxi))
-
 
     iterations += 1
