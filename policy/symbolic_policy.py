03be915ce6a507bc99c95a52be341a3bbd942836
Ethan Frank <edf32@case.edu>
2022-07-10 11:44:00 -0500

    Reduce computation by finding new experience in BFS

--- policy/symbolic_policy.py
+++ policy/symbolic_policy.py
@@ -11,6 +11,9 @@ class SymbolicPolicy(Policy):
 
         self.model = model
 
+        # Used to transfer information from the closest path to a new experience from the breadth first function
+        self.path_to_experience = None
+
     def choose_action(self, curr_state: int, is_learning: bool = True) -> int:
         # Ok, here's the current plan. Use breadth first search to try and make it to the goal state
         # (state with the max reward). If we can't make it, take a random action.
@@ -23,9 +26,10 @@ class SymbolicPolicy(Policy):
 
         print("Couldn't find path to goal")
 
-        path = self.breadth_first_search_to_untried_interaction(curr_state)
-        if len(path) > 0:
-            print(f"Found path to new experience taking action {path[-1]}, {path}")
+        # Secondary check: Were we able to find a path to a new experience?
+        if self.path_to_experience is not None:
+            path = self.path_to_experience
+            self.path_to_experience = None
             return path[-1]
 
         print("Couldn't find path to new experience")
@@ -58,6 +62,23 @@ class SymbolicPolicy(Policy):
 
             # Generate all next states
             for action in range(self.num_actions):
+                # Check if we haven't tried this action yet with the current combination of objects
+                # We do this at the same time as searching for a goal to save processing power
+
+                if self.path_to_experience is None:
+                    literals, _ = self.model.env.get_literals(curr_state)  # TODO: we already get this in compute possible transitions, is a waste
+                    for edge in literals.base_object.edges:
+                        to_object = edge.to_node.object_name[:-1]
+
+                        # Check if we haven't tried it
+                        if to_object not in self.model.experience or edge.type not in self.model.experience[to_object] or action not in self.model.experience[to_object][edge.type]:
+                            print(f"Found an experience: {str(edge.type)[14:]}-{to_object}, {action}")
+                            # Path to the current state, plus add the new action on
+                            path = self.get_path(curr_state, parents)
+                            path.insert(0, action)
+                            self.path_to_experience = path
+                            break  # Just break out of the for edge for loop since we already found one
+
                 # Compute the next state, or don't do anything if we don't know
                 transitions = self.model.compute_possible_transitions(curr_state, action)  # TODO: only need to compute literals once, not for each action
 
@@ -77,13 +98,7 @@ class SymbolicPolicy(Policy):
 
                 # Found a goal state if reward is bigger than 0
                 if self.model.get_reward(curr_state, next_state, action) > 0:
-                    path = []
-                    parent = next_state
-                    while parents[parent][0] != -1:
-                        path.append(parents[parent][1])
-                        parent = parents[parent][0]
-
-                    return path
+                    return self.get_path(next_state, parents)  # Path to that state that gives us a reward
 
                 # Otherwise, add to queue
                 q.append(next_state)
@@ -91,79 +106,10 @@ class SymbolicPolicy(Policy):
         # If we get down here, there is no path, so return empty
         return []
 
-    def breadth_first_search_to_untried_interaction(self, state: int):
-        # TODO: could integrate this into the other breadth first so it learns this while it's trying that,
-        # so we don't have to do the whole breadth first twice
-        # List of visited states
-        visited = []
+    def get_path(self, parent, parents):
+        path = []
+        while parents[parent][0] != -1:
+            path.append(parents[parent][1])
+            parent = parents[parent][0]
 
-        # Queue to store in progress states
-        q = deque()
-
-        # Dictionary of state: [it's parent, the action that led to it]
-        parents = {}
-
-        # Initialize search
-        q.append(state)
-        parents[state] = [-1, -1]
-
-        # The "on the key" state is 28686
-
-        # print("Beginning breadth first search")
-        while len(q) != 0:
-            curr_state = q.popleft()
-            visited.append(curr_state)
-
-            # print(f"Popped state: {self.model.env.get_factored_state(curr_state)}")
-
-            literals, _ = self.model.env.get_literals(curr_state)
-
-            # if curr_state == 28686:
-            #     print(f"Came from state: {state}")
-            #     print(self.model.experience["key"])
-            #     print(literals)
-
-            # Generate all next states
-            for action in range(self.num_actions):
-                # Check if we haven't tried this action yet with the current combination of objects
-
-                for edge in literals.base_object.edges:
-                    to_object = edge.to_node.object_name[:-1]
-
-                    # if curr_state == 28686 and state == 44238:
-                    #     print(edge, action)
-                    # Check if we haven't tried it
-                    if to_object not in self.model.experience or edge.type not in self.model.experience[to_object] or action not in self.model.experience[to_object][edge.type]:
-                        print(f"Found an experience: {to_object}, {str(edge.type)} {action}")
-                        # print(f"{literals}")
-                        # print(self.model.experience)
-                        path = []
-                        parent = curr_state
-                        while parents[parent][0] != -1:
-                            path.append(parents[parent][1])
-                            parent = parents[parent][0]
-
-                        path.insert(0, action)
-                        return path
-
-                # Compute the next state, or don't do anything if we don't know
-                transitions = self.model.compute_possible_transitions(curr_state, action)
-
-                if len(transitions) == 0:
-                    continue
-
-                effect = transitions[0].effect  # Assume only one effect, extract it from the transition
-                next_state = self.model.next_state(curr_state, effect)
-
-                # Already saw this state
-                if next_state in visited:
-                    continue
-
-                # Update parents for this state. Need to do before otherwise it won't be in the list when we try
-                parents[next_state] = [curr_state, action]
-
-                # Otherwise, add to queue
-                q.append(next_state)
-
-        # If we get down here, there is no path, so return empty
-        return []
+        return path
